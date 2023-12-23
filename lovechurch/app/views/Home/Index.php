cf312acafb6a8ebb79a7f6bed722f56ad9060efb
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-12-23 12:10:06 -0300

    LoveChurch

--- lovechurch/app/views/Home/Index.php
+++ lovechurch/app/views/Home/Index.php
@@ -1,1 +1,18 @@
-Olá
+<section id="section__begin">
+	<div class="container">
+		<div class="d-flex flex-wrap" class="section__begin_home">
+			<div class="col-md-6" id="letspray__background">
+				<h2>A Pray Wished</h2>
+				<hr>
+				<p class="size-15pt">Let's think about God did to us, What he do to us and What he'll do to us. Anyone in the whole world gonna make for you, what God did to you</p>
+				<p class="size-15pt">Let's get stated with one little pray of thankful and grateful</p>
+			</div>
+			<div class="col-md-6" id="column__letspray">
+				<h3 class="font-roboto size-36pt">Create you first pray</h3>
+				<a href="" class="btn_letspray">
+					Click in here 
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
