c3bb3f9ecd420e662f82e577cf25286fd053c08c
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-12-23 18:57:49 -0300

    LoveChurch

--- lovechurch/app/views/Home/Index.php
+++ lovechurch/app/views/Home/Index.php
@@ -18,11 +18,25 @@
 </section>
 <section id="section__datas">
 	<div class="container">
-		<div class="square__datas">
-			<i class="fa-solid fa-person-praying"></i>
-			<h5>Pregadores</h5>
-			<hr>
-			<p></p>
+		<div class="row">
+			<div class="col-md-4 square__datas">
+				<i class="fa-solid fa-person-praying"></i>
+				<h5 class="size-30pt">Preachers</h5>
+				<hr>
+				<p class="size-15pt">We have more than 25 preachers helping us to connect you with God</p>
+			</div>
+			<div class="col-md-4 square__datas">
+				<i class="fa-solid fa-person-praying"></i>
+				<h5 class="size-30pt">Christians</h5>
+				<hr>
+				<p class="size-15pt">You can make new christians friends here. Not be alone, please!</p>
+			</div>
+			<div class="col-md-4 square__datas">
+				<i class="fa-solid fa-person-praying"></i>
+				<h5 class="size-30pt">Psychologists</h5>
+				<hr>
+				<p class="size-15pt">It's not wrong see a Psychologist. We wanna you be strong and confident</p>
+			</div>
 		</div>
 	</div>
 </section>
