73d4479e6e0d4efcf4689acb909d6a0027925722
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-12-24 16:03:00 -0300

    Update Config.php

--- lovechurch/app/views/Grupo_Oracao/Config.php
+++ lovechurch/app/views/Grupo_Oracao/Config.php
@@ -18,7 +18,31 @@
 				</div>
 				<div class="d-flex flex-wrap justify-content-between">
 					<div>
-						<p class=""></p>
+						<p class="size-15pt">
+							Gruop's Description
+						</p>
+					</div>
+					<div>
+						<p class="size-15pt">
+							<?= $grupo[0]->descricao_grupo ?>
+						</p>
+					</div>
+				</div>
+				<div class="d-flex flex-wrap justify-content-between">
+					<div>
+						<p class="size-15pt">
+							Creation's Date
+						</p>
+					</div>
+					<div>
+						<p class="size-15pt">
+							<?= Date("d/m/Y", strtotime($grupo[0]->data_group)) ?>
+						</p>
+					</div>
+				</div>
+				<div class=>
+					<div>
+						
 					</div>
 				</div>
 			</div>
